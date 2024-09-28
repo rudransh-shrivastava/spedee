68fe2e850939055c8a9031d7e1d54952834ffe55
Gopal <gopal.lohar.dev@gmail.com>
2024-09-28 17:33:28 +0530

    add basic home page

--- frontend/src/app/_components/Navbar.tsx
+++ frontend/src/app/_components/Navbar.tsx
@@ -3,25 +3,71 @@ import Image from "next/image";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export function Navbar() {
   return (
-    <nav className="h-16 w-full border-b flex gap-4 px-4">
+    <nav className="flex h-20 w-full gap-4 border-b px-4">
       <Link href="/" className="shrink-0">
-        <div className="p-2">
+        <div className="py-4">
           <Image src="/spedee-logo.png" alt="Logo" width={48} height={48} />
         </div>
       </Link>
-      <div className="flex items-center justify-center shrink-0">
-        <span className="text-sm">Select Location</span>
+      <div className="flex shrink-0 items-center justify-center">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost" className="text-sm">
+              Select Location
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Location</DialogTitle>
+              <DialogDescription>
+                Please provide your delivery location to see products at nearby
+                store
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col items-center justify-center gap-2 py-4">
+              <Button>Use Current Location</Button>
+              <span>OR</span>
+              <Input
+                type="text"
+                placeholder="Enter your location"
+                className="w-full"
+              />
+              <div className="min-h-[10rem] p-4">No Location Found</div>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
-      <div className="flex items-center w-full shrink justify-center">
-        <div className="h-9 w-full bg-secondary rounded-lg"></div>
+      <div className="flex w-full shrink items-center justify-center">
+        <div className="h-9 w-full rounded-lg bg-secondary"></div>
       </div>
-      <div className="flex items-center justify-center"><Button variant="secondary">Login</Button></div>
-      <div className="flex items-center justify-center"><Button className="gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h440q17 0 28.5 11.5T760-320q0 17-11.5 28.5T720-280H280q-45 0-68-39.5t-2-78.5l54-98-144-304H80q-17 0-28.5-11.5T40-840q0-17 11.5-28.5T80-880h65q11 0 21 6t15 17l27 57Zm134 280h280-280Z"/></svg>
-        My Cart</Button></div>
+      <div className="flex items-center justify-center">
+        <Button variant="secondary">Login</Button>
+      </div>
+      <div className="flex items-center justify-center">
+        <Button className="gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+          >
+            <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h440q17 0 28.5 11.5T760-320q0 17-11.5 28.5T720-280H280q-45 0-68-39.5t-2-78.5l54-98-144-304H80q-17 0-28.5-11.5T40-840q0-17 11.5-28.5T80-880h65q11 0 21 6t15 17l27 57Zm134 280h280-280Z" />
+          </svg>
+          My Cart
+        </Button>
+      </div>
     </nav>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,76 @@
 import { Navbar } from "@/app/_components/Navbar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HomePage />
     </div>
+  );
+}
+
+function HomePage() {
+  return (
+    <div className="mx-auto max-w-screen-xl px-8">
+      <div className="py-4">
+        <div
+          className="h-[15rem] rounded-lg bg-[#cbff03] py-8"
+          style={{
+            backgroundImage:
+              "linear-gradient(200deg, hsl(72, 100%, 50%),hsl(72.1, 100%, 30.2%))",
+          }}
+        >
+          <div className="flex h-full flex-col gap-2 px-8">
+            <div className="flex h-full flex-col justify-center gap-4 text-background">
+              <span className="text-4xl">SPEDEE</span>
+              <span>Delivery in 15 - 45 minutes</span>
+            </div>
+            <Button className="mt-auto w-max">Shop Now</Button>
+          </div>
+        </div>
+      </div>
+      {/* <div className="py-4">
+        <div className="relative flex h-[calc(3.5rem+3.5rem+3.5rem)] flex-col items-center text-4xl font-bold">
+          <div className="absolute h-[3.5rem] py-2">BEST SELLERS</div>
+          <div className="absolute top-[calc(3.5rem)] h-[3.5rem] py-2 opacity-75">
+            BEST SELLERS
+          </div>
+          <div className="absolute top-[calc(3.5rem+3.5rem)] h-[calc(3.5rem)] py-2 opacity-50">
+            BEST SELLERS
+          </div>
+        </div>
+      </div> */}
+      <div className="py-4">
+        <ProductCard />
+      </div>
+    </div>
+  );
+}
+
+function ProductCard() {
+  return (
+    <Card className="max-w-[12rem]">
+      <CardHeader className="p-4 pb-2">
+        <Image src="/product-01.avif" alt="Bread" width={200} height={200} />
+        <CardTitle>Modern Sandwich Bread</CardTitle>
+        <CardDescription>400 g</CardDescription>
+      </CardHeader>
+      <CardContent className="p-4 pt-0">
+        <div className="flex items-center justify-between">
+          <span className="font-bold">&#8377;38</span>
+          <Button>Add</Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
