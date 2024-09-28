aea12e266202d7cdddcee659bc430347a132f7f4
Gopal <gopal.lohar.dev@gmail.com>
2024-09-28 15:33:20 +0530

    fix errors

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Course Crafter
-
-An platform that helps transform video playlists into fully-featured interactive courses. Add descriptions, transcripts, quizzes, and more to enhance the learning experience. Built with TypeScript, React, Node.js, and MongoDB.
+# Spedee
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,12 @@
 import express, { Request, Response } from "express";
-import path from "path";
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const app = express();
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 const PORT = process.env.PORT || 3001;
-
-app.get("/", (_, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
 
 app.get("/api/v1/", (_, res) => {
   res.json({ message: "hii" });
