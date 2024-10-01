537a75842b9d4b341720e505e15800c83d77764b
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-10-01 15:04:38 +0530

    rename publish->upload

--- src/app/api/v1/vendor/product/upload/route.ts
+++ src/app/api/v1/vendor/product/upload/route.ts
@@ -3,12 +3,27 @@ import { connectDB } from "@/lib/mongodb";
 import { getServerSession } from "next-auth";
 import { NextRequest } from "next/server";
 import z from "zod";
+// import multer from "multer";
+
 /*
 TODO: Left out properties to save
 - image
 - otherImages
 - vendorEmail
 */
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
+
+// const s3 = new AWS.S3();
+
+// Configure multer storage
+// const storage = multer.memoryStorage(); // Use memory storage to avoid saving files locally
+// const upload = multer({ storage: storage });
+
 const productSchema = z.object({
   name: z.string(),
   description: z.string(),
@@ -20,10 +35,14 @@ const productSchema = z.object({
   bestSeller: z.boolean(),
   bestSellerPriority: z.number(),
 });
+export const config = {
+  api: {
+    bodyParser: false, // Disable Next.js body parsing to handle multipart/form-data
+  },
+};
 
 export async function POST(req: NextRequest) {
   await connectDB();
-  const data = await req.json();
   const session = await getServerSession(authOptions);
 
   if (!session) {
