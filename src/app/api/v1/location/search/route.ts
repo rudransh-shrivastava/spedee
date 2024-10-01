a7d43f020ddc74cf35c93f9095d0800c7c2c4d0f
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-10-01 15:46:48 +0530

    add apis to search multiple or single locations

--- src/app/api/v1/location/search/route.ts
+++ src/app/api/v1/location/search/route.ts
@@ -1,12 +1,10 @@
-import { connectDB } from "@/lib/mongodb";
 import { NextRequest } from "next/server";
 import { Client } from "@googlemaps/google-maps-services-js";
 
 export async function POST(req: NextRequest) {
-  await connectDB();
-  const data = await req.json();
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.get("query");
 
-  const query = data.query;
   if (!query) {
     return Response.json(
       { message: "Missing query parameter." },
@@ -16,26 +14,6 @@ export async function POST(req: NextRequest) {
 
   const apiKey = process.env.GOOGLE_MAPS_API_KEY;
   const client = new Client({});
-
-  try {
-    const response = await client.placeAutocomplete({
-      params: {
-        input: query,
-        location: { lat: 28.6139, lng: 77.209 }, // Latitude and Longitude for Delhi
-        radius: 100000, // 100 kilometers radius
-        key: apiKey as string,
-      },
-    });
-
-    return Response.json(response.data);
-  } catch (error) {
-    if (error instanceof Error) {
-      return Response.json({ message: error.message }, { status: 500 });
-    } else {
-      return Response.json(
-        { message: "An unknown error occurred." },
-        { status: 500 }
-      );
-    }
-  }
+  // TODO: implement
+  return Response.json({ message: "testing " });
 }
