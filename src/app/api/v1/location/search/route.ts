c9bdab135df0c55e4f50684626afca6ee0c1a63f
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-10-01 15:28:40 +0530

    add google maps api

---
+++ src/app/api/v1/location/search/route.ts
@@ -0,0 +1,47 @@
+import { connectDB } from "@/lib/mongodb";
+import { NextRequest } from "next/server";
+import { Client } from "@googlemaps/google-maps-services-js";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export async function POST(req: NextRequest) {
+  await connectDB();
+  const data = await req.json();
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return Response.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const query = data.query;
+  if (!query) {
+    return Response.json(
+      { message: "Missing query parameter." },
+      { status: 400 }
+    );
+  }
+
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const client = new Client({});
+
+  try {
+    const response = await client.placeAutocomplete({
+      params: {
+        input: query,
+        location: { lat: 28.6139, lng: 77.209 }, // Latitude and Longitude for Delhi
+        radius: 100000, // 100 kilometers radius
+        key: apiKey as string,
+      },
+    });
+
+    return Response.json(response.data);
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ message: error.message }, { status: 500 });
+    } else {
+      return Response.json(
+        { message: "An unknown error occurred." },
+        { status: 500 }
+      );
+    }
+  }
+}
