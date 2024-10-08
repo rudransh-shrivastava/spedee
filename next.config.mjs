1a952905b9d19446d778eac6a7cd17ca9c460882
Gopal <gopal.lohar.dev@gmail.com>
2024-10-08 15:22:00 +0530

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "lh3.googleusercontent.com"],
+    // ["localhost", "lh3.googleusercontent.com"
+    remotePatterns: [
+      { protocol: "http", hostname: "localhost", port: "3000" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
   },
 };
 
