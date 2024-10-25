d1ef12fce6d8d4963e9136bb5ed43320572877f4
Gopal <gopal.lohar.dev@gmail.com>
2024-10-25 10:43:19 +0530

    remove old code for variants in product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -10,7 +10,7 @@ import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function ProductPage({
   params: { id },
@@ -20,25 +20,14 @@ export default function ProductPage({
   const { status: productStatus, data: product } = useQuery(
     queries.product(id)
   );
-  const { status: cartQueryStatus, data: cartProducts } = useQuery(
-    queries.cart
-  );
   return (
-    <LoadingData status={[productStatus, cartQueryStatus]}>
-      {product && cartProducts && (
-        <ProductComponent product={product} cartProducts={cartProducts} />
-      )}
+    <LoadingData status={[productStatus]}>
+      {product && <ProductComponent product={product} />}
     </LoadingData>
   );
 }
 
-function ProductComponent({
-  product,
-  cartProducts,
-}: {
-  product: ProductType;
-  cartProducts: { product: ProductType; quantity: number }[];
-}) {
+function ProductComponent({ product }: { product: ProductType }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
@@ -63,23 +52,27 @@ function ProductComponent({
     [updateVariantURLParams]
   );
 
-  const [currentVariant, setCurrentVariant] = useState<VariantType>(() => {
-    const currentVariant = product.variants.find((variant) => {
-      const attributes = variant.attributes;
-      Object.keys(product.attributes).every((key) => {
-        return attributes[key] === searchParams.get(key);
+  const [currentVariant, setCurrentVariant] = useState<VariantType>(
+    product.variants[0]
+  );
+
+  useEffect(() => {
+    setCurrentVariant(() => {
+      const currentVariant = product.variants.find((variant) => {
+        const attributes = variant.attributes;
+        Object.keys(product.attributes).every((key) => {
+          return attributes[key] === searchParams.get(key);
+        });
       });
+      if (currentVariant) return currentVariant;
+      updateVariantURLParams(product.variants[0]);
+      return product.variants[0];
     });
-    if (currentVariant) return currentVariant;
-    updateVariantURLParams(product.variants[0]);
-    return product.variants[0];
-  });
+  }, [product, searchParams, updateVariantURLParams]);
 
   const [currentProductImage, setCurrentProductImage] = useState(
     currentVariant.image
   );
-
-  const isSale = currentVariant.priceInPaise > currentVariant.salePriceInPaise;
 
   return (
     <div className="grid grid-cols-[34rem,auto]">
@@ -97,103 +90,41 @@ function ProductComponent({
               setCurrentProductImage(currentVariant.image);
             }}
           />
-          {product &&
-            currentVariant.otherImages.map((otherImage, index) => {
-              return (
-                <ProductImageCard
-                  alt={product.name}
-                  url={otherImage}
-                  className={
-                    otherImage === currentProductImage ? "border-primary" : ""
-                  }
-                  onMouseOver={() => {
-                    setCurrentProductImage(otherImage);
-                  }}
-                  key={index}
-                />
-              );
-            })}
+          {currentVariant.otherImages.map((otherImage, index) => {
+            return (
+              <ProductImageCard
+                alt={product.name}
+                url={otherImage}
+                className={
+                  otherImage === currentProductImage ? "border-primary" : ""
+                }
+                onMouseOver={() => {
+                  setCurrentProductImage(otherImage);
+                }}
+                key={index}
+              />
+            );
+          })}
         </div>
-        {product && (
-          <div className="relative size-[25rem] overflow-hidden">
-            <Image
-              src={currentProductImage}
-              width={500}
-              height={500}
-              alt={product.name}
-              className="absolute block size-full object-contain object-center"
-            />
-          </div>
-        )}
+        <div className="relative size-[25rem] overflow-hidden">
+          <Image
+            src={currentProductImage}
+            width={500}
+            height={500}
+            alt={product.name}
+            className="absolute block size-full object-contain object-center"
+          />
+        </div>
       </div>
-      <div className="space-y-2 p-4">
-        <h1 className="text-2xl font-medium">{product?.name}</h1>
-        {isSale && <div className="text-green-500">Special Price in Sale</div>}
-        <div
-          className={cn(
-            "flex items-end gap-2",
-            isSale ? "grid-cols-[1fr,auto]" : ""
-          )}
-        >
-          {isSale && (
-            <span className="relative inline-block text-2xl font-bold">
-              &#8377;{currentVariant.salePriceInPaise}
-            </span>
-          )}
-          <span
-            className={cn(
-              "relative inline-block px-1 text-2xl font-bold text-foreground",
-              isSale ? "text-lg opacity-55" : ""
-            )}
-          >
-            {isSale && (
-              <span
-                className={cn(
-                  "absolute left-0 top-[calc(50%-1px)] block h-0.5 w-[0%] bg-current",
-                  isSale ? "w-full" : ""
-                )}
-              ></span>
-            )}
-            &#8377;{currentVariant.priceInPaise}
-          </span>
-        </div>
-        <p className="">{product?.description}</p>
-        <div>
-          {product.variants.length &&
-            Object.keys(product.attributes).map((key, index) => (
-              <div key={index}>
-                <div>{key}</div>
-                <div className="flex flex-wrap gap-2">
-                  {product.variants.map((variant, index) => (
-                    <div
-                      key={index}
-                      onClick={() => {
-                        switchVariant(variant);
-                      }}
-                      className={cn(
-                        "flex cursor-pointer flex-col items-center gap-1 border-2 border-transparent bg-secondary p-1 pb-0",
-                        searchParams.get(key) === variant.attributes[key]
-                          ? "border-primary"
-                          : ""
-                      )}
-                    >
-                      <div className="size-12">
-                        <Image
-                          src={product.variants[0].image}
-                          width={48}
-                          height={48}
-                          alt=""
-                          className="size-12 object-cover"
-                        />
-                      </div>
-                      <div>{variant.attributes[key]}</div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-        </div>
 
+      <div className="space-y-2 p-4">
+        <h1 className="text-3xl font-bold opacity-80">{product.name}</h1>
+        <ProductPrice
+          price={currentVariant.priceInPaise}
+          salePrice={currentVariant.salePriceInPaise}
+        />
+        <p className="font-medium text-foreground/60">{product?.description}</p>
+        <ProductVariants />
         <div className="flex gap-2 py-2">
           <AddToCartButton product={product} className="w-full" />
           <Button className="w-full" asChild>
@@ -215,7 +146,6 @@ function ProductImageCard({
   alt: string;
   className?: string;
 }) {
-  console.log(url);
   return (
     <div
       className={cn(
@@ -233,4 +163,51 @@ function ProductImageCard({
       />
     </div>
   );
+}
+
+function ProductPrice({
+  price,
+  salePrice,
+}: {
+  price: number;
+  salePrice: number;
+}) {
+  const isSale = price > salePrice;
+  return (
+    <>
+      {isSale && <div className="text-green-500">Special Price in Sale</div>}
+      <div
+        className={cn(
+          "flex items-end gap-2",
+          isSale ? "grid-cols-[1fr,auto]" : ""
+        )}
+      >
+        {isSale && (
+          <span className="relative inline-block bg-opacity-80 text-2xl font-bold">
+            &#8377;{salePrice}
+          </span>
+        )}
+        <span
+          className={cn(
+            "relative inline-block px-1 text-2xl font-bold text-foreground",
+            isSale ? "text-lg opacity-55" : ""
+          )}
+        >
+          {isSale && (
+            <span
+              className={cn(
+                "absolute left-0 top-[calc(50%-1px)] block h-0.5 w-[0%] bg-current",
+                isSale ? "w-full" : ""
+              )}
+            ></span>
+          )}
+          &#8377;{price}
+        </span>
+      </div>
+    </>
+  );
+}
+
+function ProductVariants() {
+  return "";
 }
