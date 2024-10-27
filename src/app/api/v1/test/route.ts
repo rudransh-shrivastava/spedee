98bc8eed66288192e022050742b0de6487b5dd4d
Rudransh Shrivastava <rudransh.shr@gmail.com>
2024-10-27 19:33:54 +0530

    add amazon ses

--- src/app/api/v1/test/route.ts
+++ src/app/api/v1/test/route.ts
@@ -1,27 +1,3 @@
-import nodemailer from "nodemailer";
 export async function GET() {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "@gmail.com",
-      pass: "",
-    },
-  });
-  async function sendOrderEmail() {
-    const mailOptions = {
-      from: "@gmail.com",
-      to: "@gmail.com",
-      subject: "Order Confirmation",
-      text: `Thank you for your order, ! Your order details: $}`,
-    };
-
-    try {
-      await transporter.sendMail(mailOptions);
-      console.log("Order email sent successfully");
-    } catch (error) {
-      console.error("Error sending order email:", error);
-    }
-  }
-  sendOrderEmail();
   return new Response("Hello World");
 }
